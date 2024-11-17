7bd6eb3328bac674e1d469d62d9fb350a6315010
Mislav Balunovic <mislav.balunovic@gmail.com>
2024-11-17 11:52:28 +0100

    add vision and free-form llm detectors

--- invariant_runner/scorers/utils/llm.py
+++ invariant_runner/scorers/utils/llm.py
@@ -1,6 +1,14 @@
 import json
 import openai
+from pydantic import BaseModel
 
+
+class DetectionPair(BaseModel):
+    line: int
+    substring: str
+
+class Detections(BaseModel):
+    detections: list[DetectionPair]
 
 PROMPT_TEMPLATE = """Your goal is to classify the text provided by the user using the following classification rule.
 {prompt}
@@ -8,12 +16,37 @@ PROMPT_TEMPLATE = """Your goal is to classify the text provided by the user usin
 Call the function `option_selector` with the best of the following options as the argument: {options}
 """
 
+PROMPT_TEMPLATE_VISION = """Your goal is to classify the image provided by the user using the following classification rule.
+{prompt}
+
+Call the function `option_selector` with the best of the following options as the argument: {options}
+"""
+
+LLM_DETECTOR_PROMPT_TEMPLATE = """Your goal is to extract parts of the text the match the predicate rule. Here is one example:
+
+Predicate rule: 
+cities in Switzerland
+
+Text:
+1| I arrived to Zurich last week by train from Munich.
+2| I am going to visit Geneva next week, and Bern the week after.
+3| After Bern, I am going to Paris, France.
+
+Detections:
+[("1", "Zurich"), ("2", "Geneva"), ("2", "Bern"), ("3", "Bern")]
+
+Use the following predicate rule to find the detections in the next user message:
+{predicate_rule}
+"""
 
 class LLM_Classifier:
 
-    def __init__(self, model: str, prompt: str, options: list[str]):
+    def __init__(self, model: str, prompt: str, options: list[str], vision: bool = False):
         self.model = model
-        self.prompt = PROMPT_TEMPLATE.format(prompt=prompt, options=",".join(options))
+        if vision:
+            self.prompt = PROMPT_TEMPLATE_VISION.format(prompt=prompt, options=",".join(options))
+        else:
+            self.prompt = PROMPT_TEMPLATE.format(prompt=prompt, options=",".join(options))
         self.options = options
         self.client = openai.OpenAI()
 
@@ -33,13 +66,45 @@ class LLM_Classifier:
             }
         }]
 
-
-    def classify(self, text: str) -> str:
-        prompt = self.prompt.format(text=text)
+    def classify_vision(self, base64_image: str) -> str:
         response = self.client.chat.completions.create(
             model=self.model,
-            messages=[{"role": "system", "content": prompt}, {"role": "user", "content": text}],
+            messages=[
+                {"role": "system", "content": self.prompt}, 
+                {"role": "user", "content": [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}]}
+            ],
             tools=self.tools,
             tool_choice="required",
         )
         return json.loads(response.choices[0].message.tool_calls[0].function.arguments).get("best_option", "none")
+        
+    def classify(self, text: str) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=[{"role": "system", "content": self.prompt}, {"role": "user", "content": text}],
+            tools=self.tools,
+            tool_choice="required",
+        )
+        return json.loads(response.choices[0].message.tool_calls[0].function.arguments).get("best_option", "none")
+
+
+class LLM_Detector:
+
+    def __init__(self, model: str, predicate_rule: str):
+        self.model = model
+        self.predicate_rule = predicate_rule
+        self.client = openai.OpenAI()
+
+    def _insert_lines(self, text: str) -> str:
+        return "\n".join(f"{i}| {line}" for i, line in enumerate(text.split("\n"), 1))
+
+    def detect(self, text: str) -> bool:
+        text = self._insert_lines(text)
+        prompt = LLM_DETECTOR_PROMPT_TEMPLATE.format(predicate_rule=self.predicate_rule)
+        response = self.client.beta.chat.completions.parse(
+            model=self.model,
+            messages=[{"role": "system", "content": prompt}, {"role": "user", "content": text}],
+            response_format=Detections,
+        )
+        return response.choices[0].message.parsed
+
--- sample_tests/test_strings.py
+++ sample_tests/test_strings.py
@@ -1,7 +1,8 @@
+import base64
 import pytest
 from invariant_runner.scorers.strings import *
 from invariant_runner.scorers.base import approx
-from invariant_runner.scorers.utils.llm import LLM_Classifier
+from invariant_runner.scorers.utils.llm import LLM_Classifier, LLM_Detector, DetectionPair
 
 
 def test_levenshtein():
@@ -46,5 +47,24 @@ def test_llm():
     assert res == "de"
 
 
+def test_llm_detector():
+    text = """I like apples and carrots, but I don't like bananas.\nThe only thing better than apples are potatoes and pears."""
+    llm_detector = LLM_Detector(model="gpt-4o", predicate_rule="fruits")
+    res = llm_detector.detect(text)
+    assert res.detections[0] == DetectionPair(line=1, substring="apples")
+    assert res.detections[1] == DetectionPair(line=1, substring="bananas")
+    assert res.detections[2] == DetectionPair(line=2, substring="apples")
+    assert res.detections[3] == DetectionPair(line=2, substring="pears")
 
-        
+def test_vision_classifier():
+    with open("sample_tests/assets/Group_of_cats_resized.jpg", "rb") as image_file:
+        base64_image = base64.b64encode(image_file.read()).decode('utf-8')
+    llm_clf = LLM_Classifier(model="gpt-4o-mini", prompt="What is in the image?", options=["cats", "dogs", "birds", "none"], vision=True)
+    res = llm_clf.classify_vision(base64_image)
+    assert res == "cats"
+
+    llm_clf = LLM_Classifier(model="gpt-4o", prompt="How many cats are in the image?", options=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], vision=True)
+    res = llm_clf.classify_vision(base64_image)
+    assert res == "3"
+
+
