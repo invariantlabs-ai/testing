a2f96c24b0d9d63e189d05c42ecb4f30f60091eb
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2024-11-18 18:24:40 +0100

    notebook DOCS

--- invariant_runner/custom_types/trace.py
+++ invariant_runner/custom_types/trace.py
@@ -1,6 +1,6 @@
 """Defines an Invariant trace."""
 
-from typing import Dict, List, Any, Callable
+from typing import Any, Callable, Dict, List
 
 from pydantic import BaseModel
 
@@ -9,14 +9,19 @@ from invariant_runner.custom_types.invariant_list import InvariantList
 
 
 def iterate_tool_calls(messages: list[dict]):
-    for msg in messages:
+    for msg_i, msg in enumerate(messages):
         if msg.get("role") != "assistant":
             continue
-        for tc in msg.get("tool_calls", []):
-            yield tc
+        for tc_i, tc in enumerate(msg.get("tool_calls", [])):
+            yield (f"{msg_i}.tool_calls.{tc_i}", tc)
 
 
-def match_keyword_filter_on_tool_call(kwname: str, kwvalue: str | int | Callable, value: InvariantValue | Any, tool_call: dict) -> bool:
+def match_keyword_filter_on_tool_call(
+    kwname: str,
+    kwvalue: str | int | Callable,
+    value: InvariantValue | Any,
+    tool_call: dict,
+) -> bool:
     # redirect checks on name, arguments and id to the 'function' sub-dictionary
     # this enables checks like tool_calls(name='greet') to work
     if kwname in ["name", "arguments", "id"]:
@@ -24,50 +29,85 @@ def match_keyword_filter_on_tool_call(kwname: str, kwvalue: str | int | Callable
     return match_keyword_filter(kwname, kwvalue, value)
 
 
-def match_keyword_filter(kwname: str, kwvalue: str | int | Callable, value: InvariantValue | Any) -> bool:
+def match_keyword_filter(
+    kwname: str, kwvalue: str | int | Callable, value: InvariantValue | Any
+) -> bool:
     """
     Match a keyword filter.
-    
+
     A keyword filter such as name='value' can be one of the following:
     - a string or integer value to compare against exactly
     - a lambda function to apply to the value to check for more complex conditions
-    
+
     """
     if isinstance(value, InvariantValue):
         value = value.value
-    
+
     # compare by value or use a lambda function
     if isinstance(kwvalue, str) or isinstance(kwvalue, int):
         return kwvalue == value
     elif callable(kwvalue):
         return kwvalue(value)
-    raise ValueError(f"Cannot filter '{kwname}' with '{kwvalue}' (only str/int comparison or lambda functions are supported)")
+    raise ValueError(
+        f"Cannot filter '{kwname}' with '{kwvalue}' (only str/int comparison or lambda functions are supported)"
+    )
 
-    
+
 class Trace(BaseModel):
     """Defines an Invariant trace."""
 
     trace: List[Dict]
 
-    def messages(self, selector: int | None = None, **filterkwargs) -> List[InvariantDict]:
+    def messages(
+        self, selector: int | None = None, **filterkwargs
+    ) -> List[InvariantDict]:
         """Return the messages in the trace."""
         if type(selector) is int:
             return InvariantDict(self.trace[selector], f"{selector}")
         elif len(filterkwargs) > 0:
-            return InvariantList.from_values([InvariantDict(message, [f"{i}"]) for i, message in enumerate(self.trace) if all(
-                match_keyword_filter(kwname, kwvalue, message.get(kwname)) for kwname, kwvalue in filterkwargs.items())])
+            return InvariantList.from_values(
+                [
+                    InvariantDict(message, [f"{i}"])
+                    for i, message in enumerate(self.trace)
+                    if all(
+                        match_keyword_filter(kwname, kwvalue, message.get(kwname))
+                        for kwname, kwvalue in filterkwargs.items()
+                    )
+                ]
+            )
         else:
-            return InvariantList.from_values([InvariantDict(message, [f"{i}"]) for i, message in enumerate(self.trace)])
+            return InvariantList.from_values(
+                [
+                    InvariantDict(message, [f"{i}"])
+                    for i, message in enumerate(self.trace)
+                ]
+            )
 
-    def tool_calls(self, selector: int | None = None, **filterkwargs) -> List[InvariantDict]:
+    def tool_calls(
+        self, selector: int | None = None, **filterkwargs
+    ) -> List[InvariantDict]:
         """Return the tool calls in the trace."""
-        for tc in iterate_tool_calls(self.trace):
-            if type(selector) is int:
-                for i, tc in enumerate(iterate_tool_calls(self.trace)):
-                    if i == selector:
-                        return InvariantDict(tc, f"{i}")
-            elif len(filterkwargs) > 0:
-                return InvariantList.from_values([InvariantDict(tc, [f"{i}"]) for i, tc in enumerate(iterate_tool_calls(self.trace)) if all(
-                    match_keyword_filter_on_tool_call(kwname, kwvalue, tc.get(kwname), tc) for kwname, kwvalue in filterkwargs.items())])
-            else:
-                return InvariantList.from_values([InvariantDict(tc, [f"{i}"]) for i, tc in enumerate(iterate_tool_calls(self.trace))])
+        if type(selector) is int:
+            for i, (tc_address, tc) in enumerate(iterate_tool_calls(self.trace)):
+                if i == selector:
+                    return InvariantDict(tc, tc_address)
+        elif len(filterkwargs) > 0:
+            return InvariantList.from_values(
+                [
+                    InvariantDict(tc, [tc_address])
+                    for tc_address, tc in iterate_tool_calls(self.trace)
+                    if all(
+                        match_keyword_filter_on_tool_call(
+                            kwname, kwvalue, tc.get(kwname), tc
+                        )
+                        for kwname, kwvalue in filterkwargs.items()
+                    )
+                ]
+            )
+        else:
+            return InvariantList.from_values(
+                [
+                    InvariantDict(tc, [f"{i}"])
+                    for i, tc in iterate_tool_calls(self.trace)
+                ]
+            )
