d8d13f1476f041d8b5b4365920d3967cce6548ff
Zishan <zishan@Zishans-MacBook-Pro.local>
2024-11-19 15:37:15 +0100

    add is similar matcher

--- tests/test_is_similar.py
+++ tests/test_is_similar.py
@@ -7,10 +7,9 @@ from invariant_runner.manager import Manager
 from invariant_runner.constants import Similaritymetrics
 
 def test_is_similar_levenshtein():
-    """Test another agent response."""
+    # not passing metrics parameter, default using Levenshtein metric or pass Similaritymetrics.LEVENSHTEIN
     trace = Trace(trace=[{"role": "user", "content": "Hello there"}])
     with Manager(trace) as _:
-        # not passing metrics parameter, default using Levenshtein metric or pass Similaritymetrics.LEVENSHTEIN
         # test case: expected value is longer than actual value
         assert_that(trace.messages()[0]["content"], is_similar("hello where",0.5))
         assert_that(trace.messages()[0]["content"], is_similar("Hello there Hello there Hello there",0.4))
@@ -22,12 +21,11 @@ def test_is_similar_levenshtein():
         assert_that(trace.messages()[0]["content"], is_similar("hello aaaaa", 0.3))
         assert_that(trace.messages()[0]["content"], is_similar("hello THERe",0.9))
         assert_that(trace.messages()[0]["content"], is_similar("iiiii THERE",0.5))
-        
+
 def test_is_similar_embedding():
-    """Test another agent response."""
+    # pass Similaritymetrics.EMBEDDING as parameter
     trace = Trace(trace=[{"role": "user", "content": "Hello there"}])
     with Manager(trace) as _:
-        # pass Similaritymetrics.EMBEDDING as parameter
         # test case: similar meaning
         assert_that(trace.messages()[0]["content"], is_similar("hi there",0.8, Similaritymetrics.EMBEDDING))
         assert_that(trace.messages()[0]["content"], is_similar("how are you",0.4, Similaritymetrics.EMBEDDING))
