15a8e733af6e5340d33f5c136d340c4155758580
Hemang <hemang@invariantlabs.ai>
2024-11-26 19:11:04 +0100

    Add changes to support test results summary generation when tests are run in parallel.

--- invariant_runner/utils.py
+++ invariant_runner/utils.py
@@ -1,15 +1,17 @@
 """Utility functions for the invariant runner."""
 
 import os
+import shutil
 
 from invariant_runner.constants import (
     INVARIANT_RUNNER_TEST_RESULTS_DIR,
-    INVARIANT_TEST_RUNNER_TERMINAL_WIDTH_ENV_VAR)
+    INVARIANT_TEST_RUNNER_TERMINAL_WIDTH_ENV_VAR,
+)
 
 
-def get_test_results_file_path(dataset_name: str) -> str:
-    """Get the file path for the test results."""
-    return f"{INVARIANT_RUNNER_TEST_RESULTS_DIR}/results_for_{dataset_name}.jsonl"
+def get_test_results_directory_path(dataset_name: str) -> str:
+    """Get the directory path for the test results."""
+    return f"{INVARIANT_RUNNER_TEST_RESULTS_DIR}/results_for_{dataset_name}"
 
 
 def ssl_verification_enabled():
@@ -19,10 +21,7 @@ def ssl_verification_enabled():
 
 def terminal_width():
     """Get the terminal width."""
-    import os
-    import shutil
 
     if INVARIANT_TEST_RUNNER_TERMINAL_WIDTH_ENV_VAR in os.environ:
         return int(os.environ[INVARIANT_TEST_RUNNER_TERMINAL_WIDTH_ENV_VAR])
-    else:
-        return shutil.get_terminal_size().columns
+    return shutil.get_terminal_size().columns
