8b58ac8250d99d4683e469fb8dd81c62e3c74a85
Hemang <hemang@invariantlabs.ai>
2024-11-26 12:32:35 +0100

    Format files.

--- invariant_runner/scorers/utils/llm.py
+++ invariant_runner/scorers/utils/llm.py
@@ -53,7 +53,14 @@ logger = logging.getLogger(__name__)
 class LLMClassifier:
     """Class to classify using a language model."""
 
-    def __init__(self, model: str, prompt: str, options: list[str], vision: bool = False, default_ttl: int = 3600):
+    def __init__(
+        self,
+        model: str,
+        prompt: str,
+        options: list[str],
+        vision: bool = False,
+        default_ttl: int = 3600,
+    ):
         """
         Args:
             model (str): The language model to use.
@@ -65,44 +72,51 @@ class LLMClassifier:
         self.model = model
         if vision:
             self.prompt = PROMPT_TEMPLATE_VISION.format(
-                prompt=prompt, options=",".join(options))
+                prompt=prompt, options=",".join(options)
+            )
         else:
             self.prompt = PROMPT_TEMPLATE.format(
-                prompt=prompt, options=",".join(options))
+                prompt=prompt, options=",".join(options)
+            )
         self.options = options
         self.client = openai.OpenAI()
-        self.tools = [{
-            "type": "function",
-            "function": {
-                "name": "option_selector",
-                "description": "Selects the best option from the list of options",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "best_option": {
-                            "type": "string",
+        self.tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "option_selector",
+                    "description": "Selects the best option from the list of options",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "best_option": {
+                                "type": "string",
+                            },
                         },
                     },
-                }
+                },
             }
-        }]
+        ]
         self.cache_manager = CacheManager(
-            CACHE_DIRECTORY_LLM_CLASSIFIER, expiry=default_ttl)
+            CACHE_DIRECTORY_LLM_CLASSIFIER, expiry=default_ttl
+        )
 
     def _generate_cache_key(self, function_name: str, request_data: dict) -> str:
         """Generate a cache key for the request."""
-        return self.cache_manager.get_cache_key({
-            "function_name": function_name,
-            "request_data": request_data
-        })
+        return self.cache_manager.get_cache_key(
+            {"function_name": function_name, "request_data": request_data}
+        )
 
-    def _make_completions_create_request(self, request_data: dict, use_cached_result: bool) -> ChatCompletion:
+    def _make_completions_create_request(
+        self, request_data: dict, use_cached_result: bool
+    ) -> ChatCompletion:
         """Make a request to the language model."""
         if not use_cached_result:
             return self.client.chat.completions.create(**request_data)
 
         cache_key = self._generate_cache_key(
-            "self.client.chat.completions.create", request_data)
+            "self.client.chat.completions.create", request_data
+        )
         response = self.cache_manager.get(cache_key)
         if response:
             logger.info("Using cached response for request.")
@@ -124,17 +138,31 @@ class LLMClassifier:
             base64_image (str): The base64-encoded image to classify.
             use_cached_result (bool): Whether to use a cached result if available.
         """
-        response = self._make_completions_create_request({
-            "model": self.model,
-            "messages": [
-                {"role": "system", "content": self.prompt},
-                {"role": "user", "content": [{"type": "image_url", "image_url": {
-                    "url": f"data:image/jpeg;base64,{base64_image}"}}]}
-            ],
-            "tools": self.tools,
-            "tool_choice": "required",
-        }, use_cached_result)
-        return json.loads(response.choices[0].message.tool_calls[0].function.arguments).get("best_option", "none")
+        response = self._make_completions_create_request(
+            {
+                "model": self.model,
+                "messages": [
+                    {"role": "system", "content": self.prompt},
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/jpeg;base64,{base64_image}"
+                                },
+                            }
+                        ],
+                    },
+                ],
+                "tools": self.tools,
+                "tool_choice": "required",
+            },
+            use_cached_result,
+        )
+        return json.loads(
+            response.choices[0].message.tool_calls[0].function.arguments
+        ).get("best_option", "none")
 
     def classify(self, text: str, use_cached_result: bool = True) -> str:
         """Classify a text using the language model.
@@ -143,26 +171,33 @@ class LLMClassifier:
             text (str): The text to classify.
             use_cached_result (bool): Whether to use a cached result if available.
         """
-        response = self._make_completions_create_request({
-            "model": self.model,
-            "messages": [
-                {"role": "system", "content": self.prompt},
-                {"role": "user", "content": text}
-            ],
-            "tools": self.tools,
-            "tool_choice": "required",
-        }, use_cached_result)
-        return json.loads(response.choices[0].message.tool_calls[0].function.arguments).get("best_option", "none")
+        response = self._make_completions_create_request(
+            {
+                "model": self.model,
+                "messages": [
+                    {"role": "system", "content": self.prompt},
+                    {"role": "user", "content": text},
+                ],
+                "tools": self.tools,
+                "tool_choice": "required",
+            },
+            use_cached_result,
+        )
+        return json.loads(
+            response.choices[0].message.tool_calls[0].function.arguments
+        ).get("best_option", "none")
 
 
 class DetectionPair(BaseModel):
     """Model for a detection pair."""
+
     line: int
     substring: str
 
 
 class Detections(BaseModel):
     """Model for a list of detections."""
+
     detections: list[DetectionPair]
 
 
@@ -183,7 +218,8 @@ class LLMDetector:
         self.predicate_rule = predicate_rule
         self.client = openai.OpenAI()
         self.cache_manager = CacheManager(
-            CACHE_DIRECTORY_LLM_DETECTOR, expiry=default_ttl)
+            CACHE_DIRECTORY_LLM_DETECTOR, expiry=default_ttl
+        )
 
     def _insert_lines(self, text: str) -> str:
         return "\n".join(f"{i}| {line}" for i, line in enumerate(text.split("\n"), 1))
@@ -198,18 +234,20 @@ class LLMDetector:
 
     def _generate_cache_key(self, function_name: str, request_data: dict) -> str:
         """Generate a cache key for the request."""
-        return self.cache_manager.get_cache_key({
-            "function_name": function_name,
-            "request_data": request_data
-        })
+        return self.cache_manager.get_cache_key(
+            {"function_name": function_name, "request_data": request_data}
+        )
 
-    def _make_completions_parse_request(self, request_data: dict, use_cached_result: bool):
+    def _make_completions_parse_request(
+        self, request_data: dict, use_cached_result: bool
+    ):
         """Make a request to the language model."""
         if not use_cached_result:
             return self.client.beta.chat.completions.parse(**request_data)
 
         cache_key = self._generate_cache_key(
-            "self.client.beta.chat.completions.parse", request_data)
+            "self.client.beta.chat.completions.parse", request_data
+        )
         response = self.cache_manager.get(cache_key)
         if response:
             logger.info("Using cached response for request.")
@@ -225,7 +263,9 @@ class LLMDetector:
 
         return response
 
-    def detect(self, text: str, use_cached_result: bool = True) -> list[Tuple[str, Range]]:
+    def detect(
+        self, text: str, use_cached_result: bool = True
+    ) -> list[Tuple[str, Range]]:
         """Detect parts of the text that match the predicate rule.
 
         Args:
@@ -233,15 +273,23 @@ class LLMDetector:
             use_cached_result (bool): Whether to use a cached result if available.
         """
         formatted_text = self._insert_lines(text)
-        prompt = LLM_DETECTOR_PROMPT_TEMPLATE.format(
-            predicate_rule=self.predicate_rule)
-        response = self._make_completions_parse_request({
-            "model": self.model,
-            "messages": [
-                {"role": "system", "content": prompt},
-                {"role": "user", "content": formatted_text}
-            ],
-            "response_format": Detections,
-        }, use_cached_result)
+        prompt = LLM_DETECTOR_PROMPT_TEMPLATE.format(predicate_rule=self.predicate_rule)
+        response = self._make_completions_parse_request(
+            {
+                "model": self.model,
+                "messages": [
+                    {"role": "system", "content": prompt},
+                    {"role": "user", "content": formatted_text},
+                ],
+                "response_format": Detections,
+            },
+            use_cached_result,
+        )
         detections = response.choices[0].message.parsed
-        return [(det.substring, Range.from_line(text, det.line-1, exact_match=det.substring)) for det in detections.detections]
+        return [
+            (
+                det.substring,
+                Range.from_line(text, det.line - 1, exact_match=det.substring),
+            )
+            for det in detections.detections
+        ]
