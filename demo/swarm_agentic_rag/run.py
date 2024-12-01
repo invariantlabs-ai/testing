4ffc1bdb99600a1414732b6eab4816fdd0fb1453
Marc Fischer <mail@marcfischer.at>
2024-12-01 13:51:10 +0100

    swarm agentic rag test

---
+++ demo/swarm_agentic_rag/run.py
@@ -0,0 +1,107 @@
+from swarm import Swarm, Agent
+import json
+from pathlib import Path
+
+# mock calendar  for user
+calendar = []
+calendar.append({
+    'title': 'Meeting with John',
+    'date': '2024-05-15',
+    'time': '11:00',
+    'duration': '1:00',
+})
+calendar.append({
+    'title': 'Lunch with Sarah',
+    'date': '2024-05-15',
+    'time': '12:30',
+    'duration': '1:00',
+})
+calendar.append({
+    'title': 'Meeting with Jane',
+    'date': '2024-05-16',
+    'time': '10:00',
+    'duration': '1:00',
+})
+
+def search_calendar_events(query=None, date=None):
+    """
+    Queries the calendar for events that contain the query in their title and are on the specified date.
+    If query is None, all events are returned.
+    If date is None, all events are returned. 
+    """
+    c = calendar
+    if query is not None:
+        c = [event for event in c if query in event['title']]
+    if date is not None:
+        c = [event for event in c if event['date']==date]
+    return c 
+
+def run(system_prompt, user_query, filename):
+    trace_path = Path(__file__).parent.joinpath(filename)
+    if trace_path.exists():
+        with open(trace_path, 'r') as f:
+            trace = json.load(f)
+    else:
+        client = Swarm()
+        trace = [{"role": "user", "content": user_query}]
+        agent = Agent(
+            name="Agent A",
+            instructions=system_prompt,
+            functions=[search_calendar_events],
+        )
+        response = client.run(
+            agent=agent,
+            messages=trace,
+        )
+        trace.extend(response.messages)
+        with open(trace_path, 'w') as f:
+            json.dump(trace, f)
+    return trace
+
+def run_buggy():
+    system_prompt = """You are an AI language model who assists the user by using the given tools. \
+    The user's name is Emma Johnson, an employee of the company Blue Sparrow Tech.
+    Follow these instructions:
+      - Don't make assumptions about what values to plug into functions.
+      - Use the provided tools to try to disambiguate.
+      - If a tool says that no results are available, try with a different query."""
+      
+    user_query = "How much time do I have to go to my lunch with Sarah on 2024-05-15. Give me the result in the format 'HH:MM'."
+    return run(system_prompt, user_query, 'trace_buggy.json')
+
+def run_fixed():
+    system_prompt = """You are an AI language model who assists the user by using the given tools. \
+    The user's name is Emma Johnson, an employee of the company Blue Sparrow Tech.
+    Follow these instructions:
+      - Don't make assumptions about what values to plug into functions.
+      - If a tool says that no results are available or returns no events, try with a different query.
+      - Use the provided tools multiple times if necessary. 
+      - Do not assume that the tools are using fuzzy matching. When in doubt try multiple queries.
+      """
+      
+    user_query = "How much time do I have to go to my lunch with Sarah on 2024-05-15? Give me the result in the format 'HH:MM'."
+    return run(system_prompt, user_query, 'trace_fixed.json')
+
+
+def print_trace(trace):
+    for t in trace:
+        text = f"{t['role']}: {t['content']}"
+        if 'tool_calls' in t and t['tool_calls']:
+            calls = []
+            for call in t['tool_calls']:
+                function = call['function']
+                args = ', '.join([f"{k}={v}" for k,v in json.loads(function['arguments']).items()])
+                calls.append(f"{function['name']}({args})")
+            text += f" -> {', '.join(calls)}"
+        print(text)
+
+if __name__ == "__main__":
+    print("Buggy trace:")
+    Path(__file__).parent.joinpath('trace_buggy.json').unlink(missing_ok=True)
+    trace = run_buggy()
+    print_trace(trace)
+   
+    print("\n\nFixed trace:") 
+    Path(__file__).parent.joinpath('trace_fixed.json').unlink(missing_ok=True)
+    trace = run_fixed()
+    print_trace(trace)
---
+++ demo/swarm_agentic_rag/test.py
@@ -0,0 +1,25 @@
+from run import run_buggy, run_fixed
+import invariant.testing as it
+
+# TODO: failure in ipython notebook
+
+trace = run_fixed()
+#trace = run_buggy()
+trace = it.Trace(trace=trace)
+
+def test_used_tool():
+    with trace.as_context():
+        it.assert_true(len(trace.tool_calls()) > 0, 'The calendar tool is used to access the calendar information at least once')
+        it.assert_true(len(trace.tool_calls()) == 1, 'The calendar tool should only be used once', assertion_type='SOFT')
+     
+        # TODO: can i write this better ?
+        tool_results = [result for _, result in trace.tool_pairs()] # TODO: still has none issue
+        contains_sarah = any('Sarah' in result['content'] for result in tool_results)
+        contains_john = any('John' in result['content'] for result in tool_results)
+        it.assert_true(contains_sarah and contains_john, 'The calendar tool should return events for both Sarah and John')
+
+def test_correct():
+    with trace.as_context():
+        # TODO: no highlighting
+        it.assert_true('0:30' in trace.messages(-1)['content'])#, 'The result should be 1:00')
+
