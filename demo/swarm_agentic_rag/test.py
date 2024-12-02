bd704227463ef8118e45b115ee2991d23c617e7e
Marc Fischer <mail@marcfischer.at>
2024-12-02 14:54:13 +0100

    wip

--- demo/swarm_agentic_rag/test.py
+++ demo/swarm_agentic_rag/test.py
@@ -1,23 +1,35 @@
 from run import run_buggy, run_fixed
 import invariant.testing as it
-
-# TODO: failure in ipython notebook
-# TODO: need absolute path to run
+import invariant.testing.functional as F
+from invariant.custom_types.invariant_number import InvariantNumber
 
 trace = run_fixed()
 #trace = run_buggy()
-trace = it.Trace(trace=trace)
+trace = it.Trace(trace=trace) # TODO use from_swarm
+
+def len(iterable):
+    #return F.count(lambda x: InvariantNumber(1, x.addresses), iterable) # TODO does not work
+    return F.count(lambda x: InvariantNumber(1), iterable)
+
+def tool_results(trace):
+    return F.filter(lambda x: x['role'] == 'tool', trace.trace)
+    #return [result for _, result in trace.tool_pairs()]
 
 def test_used_tool():
     with trace.as_context():
         it.assert_true(len(trace.tool_calls()) > 0, 'The calendar tool is used to access the calendar information at least once')
         it.expect_true(len(trace.tool_calls()) == 1, 'The calendar tool should only be used once')
-     
-        # TODO: can i write this better ?
-        tool_results = [result for _, result in trace.tool_pairs()] # TODO: still has none issue
-        contains_sarah = any('Sarah' in result['content'] for result in tool_results)
-        contains_john = any('John' in result['content'] for result in tool_results)
-        it.assert_true(contains_sarah and contains_john, 'The calendar tool should return events for both Sarah and John')
+ 
+ 
+ 
+        #tool_results = F.filter(lambda x: x['role'] == 'tool', trace.trace) 
+        #print(tool_results[0]['content'], type(tool_results[0]['content']))
+        #count_sarah = F.count(lambda x: 'Sarah' in x['content'], tool_results(trace))
+        #count_john = F.count(lambda x: 'John' in x['content'], tool_results(trace))
+
+        count_sarah = F.match('Sarah', F.map(lambda tcr: tcr['content'], tool_results(trace)))
+        count_john = F.match('John', F.map(lambda tcr: tcr['content'], tool_results(trace)))
+        it.assert_true(len(count_sarah) >= 1 and len(count_john) >= 1, 'The calendar tool should return events for both Sarah and John')
 
 def test_correct():
     with trace.as_context():
