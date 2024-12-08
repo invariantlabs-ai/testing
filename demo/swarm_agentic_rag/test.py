14622d0fbdab52dc1d2bb22bbd5459f696c3a9f5
Marc Fischer <mail@marcfischer.at>
2024-12-08 22:18:14 +0100

    test improvement

--- demo/swarm_agentic_rag/test.py
+++ demo/swarm_agentic_rag/test.py
@@ -4,12 +4,12 @@ import invariant.testing.functional as F
 
 trace = run_fixed()
 #trace = run_buggy()
-trace = it.Trace.from_swarm(trace)
+trace = it.TraceFactory.from_swarm(trace)
 
 def test_used_tool():
     with trace.as_context():
-        it.assert_true(len(trace.tool_calls()) > 0, 'The calendar tool is used to access the calendar information at least once')
-        it.expect_true(len(trace.tool_calls()) == 1, 'The calendar tool should only be used once')
+        it.assert_true(F.len(trace.tool_calls()) > 0, 'The calendar tool is used to access the calendar information at least once')
+        it.expect_true(F.len(trace.tool_calls()) == 1, 'The calendar tool should only be used once')
 
         count_sarah = F.len(trace.messages(role='tool', content=lambda c: 'Sarah' in c))
         count_john = F.len(trace.messages(role='tool', content=lambda c: 'John' in c))
