b95b2440434e4ee23f7e3f14f6c76fb8f510a591
Hemang <hemang@invariantlabs.ai>
2024-12-08 22:32:02 +0100

    Update the Capital finder agent test for Swarm.

--- sample_tests/swarm/capital_finder_agent/test_capital_finder_agent.py
+++ sample_tests/swarm/capital_finder_agent/test_capital_finder_agent.py
@@ -1,11 +1,11 @@
 """Tests for the capital_finder_agent"""
 
+import invariant.testing.functional as F
 import pytest
 from invariant.testing import SwarmWrapper, assert_equals, assert_true
 from swarm import Swarm
 
 from .capital_finder_agent import create_agent
-import invariant.testing.functional as F
 
 
 @pytest.fixture(name="swarm_wrapper", scope="module")
@@ -25,7 +25,7 @@ def test_capital_finder_agent_when_capital_found(swarm_wrapper):
     trace = SwarmWrapper.to_invariant_trace(response)
 
     with trace.as_context():
-        get_capital_tool_calls = trace.tool_calls(name=lambda n: n == "get_capital")
+        get_capital_tool_calls = trace.tool_calls(name="get_capital")
         assert_true(F.len(get_capital_tool_calls) == 1)
         assert_equals(
             "France", get_capital_tool_calls[0]["function"]["arguments"]["country_name"]
@@ -35,7 +35,7 @@ def test_capital_finder_agent_when_capital_found(swarm_wrapper):
 
 
 def test_capital_finder_agent_when_capital_not_found(swarm_wrapper):
-    """Test the capital finder agent when the capital is found."""
+    """Test the capital finder agent when the capital is not found."""
     agent = create_agent()
     messages = [{"role": "user", "content": "What is the capital of Spain?"}]
     response = swarm_wrapper.run(
@@ -45,14 +45,14 @@ def test_capital_finder_agent_when_capital_not_found(swarm_wrapper):
     trace = SwarmWrapper.to_invariant_trace(response)
 
     with trace.as_context():
-        get_capital_tool_calls = trace.tool_calls(name=lambda n: n == "get_capital")
+        get_capital_tool_calls = trace.tool_calls(name="get_capital")
         assert_true(F.len(get_capital_tool_calls) == 1)
         assert_equals(
             "Spain", get_capital_tool_calls[0]["function"]["arguments"]["country_name"]
         )
 
-        messages_with_role_tool = trace.messages(role="tool")
-        assert_true(F.len(messages_with_role_tool) == 1)
-        assert_true("not_found" in messages_with_role_tool[0]["content"])
+        tool_outputs = trace.tool_outputs(tool_name='get_capital')
+        assert_true(F.len(tool_outputs) == 1)
+        assert_true("not_found" in tool_outputs[0]["content"])
 
         assert_true("Madrid" not in trace.messages(-1)["content"])
